76fb9dc74284d6a6e26fdd58e0d33f7a6f9cb64a
HONGEUNTAEK <150913784+HONGEUNTAEK@users.noreply.github.com>
2025-11-05 17:19:04 +0900

    main end 90%

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-// 메인 배너용 Swiper
-var swiperMain = new Swiper(".mySwiper", {
-  spaceBetween: 0,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".mySwiper .swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".mySwiper .swiper-button-next",
-    prevEl: ".mySwiper .swiper-button-prev",
-  },
-});
+    // 메인 배너 Swiper
+    var swiperMain = new Swiper(".mySwiper", {
+      spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".mySwiper .swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".main-next",
+        prevEl: ".main-prev",
+      },
+    });
 
-// 제품 카드용 Swiper
-var swiperProduct = new Swiper(".productSwiper", {
-  slidesPerView: 3,
-  spaceBetween: 40,
-  loop: true,
-  navigation: {
-    nextEl: ".productSwiper .swiper-button-next",
-    prevEl: ".productSwiper .swiper-button-prev",
-  },
-  breakpoints: {
-    1024: { slidesPerView: 3 },
-    768: { slidesPerView: 2 },
-    480: { slidesPerView: 1 },
-  },
-});
+    // 제품 Swiper
+    var swiperProduct = new Swiper(".productSwiper", {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".productSwiper .swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".product-next",
+        prevEl: ".product-prev",
+      },
+    });
